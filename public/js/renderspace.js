1a56890f6ec92fd5c656f39db7f56bd783b1d4fe
Yury Kornev <korneff@gmail.com>
2019-03-30 01:11:20 +0100

    i-20190330-h

--- public/js/renderspace.js
+++ public/js/renderspace.js
@@ -1,3 +1,15 @@
 'use strict';
 
-document.getElementById('pWelcomTitle').textContent=`Welcome to room ${roomId}, ${userNick}`;
+document.getElementById('spaceTitleId').textContent=`Project K5. Room: ${roomId}, user: ${userNick}`;
+
+function videoTest(){
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then((stream)=>{
+        document.getElementById('localVideo').srcObject=stream;
+        document.getElementById('video1').srcObject=stream;
+        document.getElementById('video2').srcObject=stream;
+    })
+    .catch ((err)=>{
+        console.log(err);
+    });
+}
